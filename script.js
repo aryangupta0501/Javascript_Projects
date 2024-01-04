3a68e40c20b6ebbf1ad17e11d3717ccb58dd52c5
aryangupta0501 <155458815+aryangupta0501@users.noreply.github.com>
2024-01-04 22:54:43 +0530

    Add files via upload
    This is Javascript project 2. This project changes the background colour every time you press the button

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-const button = document.getElementById('btn')
-const heading = document.getElementById('head')
-const span = document.getElementById('spaan')
-button.addEventListener('click',()=>{
-    var call_fun=prompt('alert!')
-    span.textContent=call_fun
+const btn= document.getElementById('openModal')
+const model= document.querySelector('.modal')
+btn.addEventListener('click',()=>{
+    model.classList.add('open')
+})
+btn.addEventListener('click',()=>{
+    model.classList.remove('open')
 })
